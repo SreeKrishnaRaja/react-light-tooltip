eaff4f1a4607c98ee3e6178cea7ebacd84820902
Sree Krishna Raja <sreekrishnaraja.kp@gmail.com>
2018-07-11 00:38:38 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './balloon.css';
+
+export default function Tooltip(props) {
+  const { parent } = props;
+  const parentProps = {
+    ...parent.props,
+    'data-balloon': props.tooltip,
+    'data-balloon-pos': props.position,
+    'data-balloon-length': props.length,
+  };
+  return React.createElement(
+    parent.type,
+    props.disabled ? {...parent.props} : {...parentProps},
+    [...parent.props.children]
+  );
+}
+
+Tooltip.defaultProps = {
+  position: 'up',
+  disabled: false,
+  length: 'small',
+}
